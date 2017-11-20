3fdc5cd168f4ea058f6f14a9a66ee0647c2e62ac
Andrey Salikov <borz.salikov@gmail.com>
2017-11-20 20:50:52 +0300

    Добавил новые задачи

---
+++ age.py
@@ -0,0 +1,3 @@
+age = int(input('Сколько вам лет: '))
+birth_year = 2017 - age
+print("Вы родились в " + str(birth_year) + " году!")
---
+++ example.py
@@ -0,0 +1,12 @@
+def get_vat(payment, percent=18):
+    try:    
+        payment = float(payment)
+        percent = int(percent)
+        vat = payment / 100 * percent
+        vat = round(vat,2)
+        return "Сумма НДС {} рублей".format(vat)
+    except (TypeError,ValueError):
+        return 'Ошибка, проверьте воод'     
+
+result = get_vat(10000, 18)
+print(result)
